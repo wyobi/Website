1abb0968b02f4b86c44d207937b6c6ee07292ddf
Fanie Oosthuysen <fanie.oosthuysen@gmail.com>
2016-03-28 11:56:19 +0200

    First Commit

---
+++ mailchimp/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+// API Key - see http://admin.mailchimp.com/account/api
+$apikey			= 'ea082345eec6410676f9788143954f08-us12'; // YOUR MAILCHIMP APIKEY
+    
+// A List Id to run examples against. use lists() to view all
+// Also, login to MC account, go to List, then List Tools, and look for the List ID entry
+$listId			= 'b94d888b7b'; // YOUR MAILCHIMP LIST ID - see lists() method
+    
+// A Campaign Id to run examples against. use campaigns() to view all
+$campaignId		= ''; // YOUR MAILCHIMP CAMPAIGN ID - see campaigns() method
+
+// just used in xml-rpc examples
+$apiUrl			= 'http://api.mailchimp.com/1.3/';
+    
+?>
