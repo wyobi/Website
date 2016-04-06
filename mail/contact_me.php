9db69788cd42cd2a10dbf25827c756b2ff23b46d
Fanie Oosthuysen <fanie.oosthuysen@gmail.com>
2016-04-06 22:52:15 +0200

    testing

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -20,9 +20,10 @@ $email_subject = "Contact form submitted by:  $name";
 $email_body = "You have received a new message. \n\n".
 				  " Here are the details:\n \nName: $name \n ".
 				  "Email: $email_address\n Message \n $message";
-$headers = "";
-//$headers = "From: info@wyobi.com\n";
-//$headers .= "Reply-To: $email_address";	
+$headers = "From: admin@wyobi.com" . " " .
+"Reply-To: $email_address" . " " .
+"X-Mailer: PHP/" . phpversion();
+
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
